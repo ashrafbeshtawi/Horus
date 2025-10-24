09152f8a3d5bb46892ab346c40ae091652116d7d
Ashraf Beshtawi <beshtawi.ashraf@gmail.com>
2025-10-24 18:48:56 +0200

    nice menu

--- Nuxt/pages/index.vue
+++ Nuxt/pages/index.vue
@@ -350,6 +350,7 @@ export default {
       });
     },
     
+    
     onMouseClick(event) {
       this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
       this.mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
